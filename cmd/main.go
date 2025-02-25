6758736e274cd1a1367fea29b2c9069a26a92f2d
gonyango <vinolia.b.19@gmail.com>
2025-02-25 05:21:36 +0300

    feat: Identify url from the arguements

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello there")
+	if len(os.Args) < 2 {
+		fmt.Println("Invalid number of arguements\nUsage: go run . <URL>")
+		return
+	}
+
+	args := os.Args[1:]
+	startTime := time.Now()
+	url := ""
+	fmt.Println("Start at:", startTime.Format("2006-01-02 15:04:05"))
+
+	for _, arg := range args {
+		if strings.Contains(arg, "https:") {
+			url = arg
+		}
+	}
+
+	fmt.Println(url)
+
 }
