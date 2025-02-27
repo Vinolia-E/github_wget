962e12d77c7c1fe98cf5cfb208eda8328592c913
gonyango <vinolia.b.19@gmail.com>
2025-02-27 04:12:00 +0300

    feat: add call to ParseFlag function

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 	"time"
+	"wget/cmd/functions"
 )
+
+type ArgValues struct {
+	URL []string
+	Flag string
+	OutputFile string
+	Path string
+	Background bool
+	InputFile string
+	IsMirror bool
+	RateLimit string
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -15,15 +26,13 @@ func main() {
 
 	args := os.Args[1:]
 	startTime := time.Now()
-	url := ""
+	// url := ""
 	fmt.Println("Start at:", startTime.Format("2006-01-02 15:04:05"))
 
-	for _, arg := range args {
-		if strings.Contains(arg, "https:") {
-			url = arg
-		}
-	}
+	functions.ParseFlags(args)
 
-	fmt.Println(url)
 
+	fmt.Println("End!")
 }
+
+
