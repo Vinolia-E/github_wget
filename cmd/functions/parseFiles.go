bcbdb2592848fe59182aa9badd6568444169a69c
vandisi <vinolia.b.19@gmail.com>
2025-03-21 07:30:10 +0300

    fix: Change identification of the P flag from lowercase to uppercase

--- cmd/functions/parseFiles.go
+++ cmd/functions/parseFiles.go
@@ -5,11 +5,11 @@ import (
 	"strings"
 )
 
-func (input *ArgValues)ParseFlags(args []string) {
+func (input *ArgValues) ParseFlags(args []string) {
 	for _, arg := range args {
 		if arg == "-B" {
 			input.Background = true
-			fmt.Println("Output downloaded to the background and redirected to a log file")
+
 		} else if strings.HasPrefix(arg, "-O=") {
 			input.OutputFile = arg[len("-O="):]
 			// fmt.Println("-0 flag passed")
@@ -17,19 +17,19 @@ func (input *ArgValues)ParseFlags(args []string) {
 			input.HasFlag = true
 			input.URL = arg
 			// fmt.Println("valid url")
-		} else if strings.HasPrefix(arg, "-p=") {
-			input.Path = arg[len("-p="):]
-			// fmt.Println("Path to save file")
+		} else if strings.HasPrefix(arg, "-P=") {
+			input.Path = arg[len("-P="):]
+			// input.OutputFile = args[i+1]
+			fmt.Println("Path to save file")
 		} else if strings.HasPrefix(arg, "--rate-limit=") {
 			input.RateLimit = arg[len("--rate-limit="):]
 			fmt.Printf("download speed limit is : %v", input.RateLimit)
 		} else if arg == "--mirror" {
 			input.IsMirror = true
 			fmt.Println("Instance of mirror")
-		} else if strings.HasPrefix(arg, "-i="){
+		} else if strings.HasPrefix(arg, "-i=") {
 			input.InputFile = arg[len("-i="):]
 			fmt.Println("Downlowd different files from a file")
 		}
-		// fmt.Println("Hello, complete the perse function")
 	}
 }
