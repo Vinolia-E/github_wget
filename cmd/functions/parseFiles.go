00dad09623bd5d15c641c37212aa81c6230b5e8d
Vinolia-E <vinolia.b.19@gmail.com>
2025-03-26 07:45:25 +0300

    fix: Correct method call for URL

--- cmd/functions/parseFiles.go
+++ cmd/functions/parseFiles.go
@@ -15,7 +15,8 @@ func (input *ArgValues) ParseFlags(args []string) {
 			// fmt.Println("-0 flag passed")
 		} else if strings.HasPrefix(arg, "https://") || strings.HasPrefix(arg, "http://") || strings.HasPrefix(arg, "ftp://") {
 			input.HasFlag = true
-			input.URL = append(input.URL, arg) // arg
+			input.URL = arg
+			// input.URL = append(input.URL, arg) // arg
 			// fmt.Println("valid url")
 		} else if strings.HasPrefix(arg, "-P=") {
 			input.Path = arg[len("-P="):]
