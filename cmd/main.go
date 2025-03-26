3d0ce65de911719ca7ad7263f7cc7a4acd302791
Vinolia-E <vinolia.b.19@gmail.com>
2025-03-26 10:36:44 +0300

    feat: Introduce goroutine to download multiple files co-currently

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 	"path"
-	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -24,7 +23,6 @@ func main() {
 	flags := &functions.ArgValues{}
 	flags.ParseFlags(args)
 
-	// if flags.URL == nil {
 	if flags.URL == nil && flags.InputFile == "" {
 		fmt.Println("Error: No URL provided")
 		return
@@ -32,16 +30,11 @@ func main() {
 
 	if flags.InputFile != "" {
 		inputs := functions.ReadInputFile(flags.InputFile)
-		fmt.Println(inputs)
-
-		// flags.URL = url
 		flags.URL = append(flags.URL, inputs...)
 	}
 
 	// Handle background mode
 	if flags.Background {
-		// functions.BackgroundMode()
-
 		fmt.Println("Output will be written to 'wget-log'.")
 		file, err := os.OpenFile("wget-log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
 		if err != nil {
@@ -56,211 +49,87 @@ func main() {
 	// Print start time
 	startTime := time.Now()
 	fmt.Printf("start at %s\n", startTime.Format("2006-01-02 15:04:05"))
-	fmt.Print("sending request, awaiting response... ")
+	fmt.Print("sending request, awaiting response... \n")
 
-	// StartDownloads(args, flags.URL, flags.Path)
-	// Send GET request
+	// Use a WaitGroup for async downloads
+	var wg sync.WaitGroup
 
 	for _, url := range flags.URL {
-		fmt.Println("THIS IS THE URL", url)
-		response, err := http.Get(url) //(flags.URL)
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-		defer response.Body.Close()
-
-		// Print response status
-		fmt.Printf("status %s\n", response.Status)
-		if response.StatusCode != http.StatusOK {
-			fmt.Println("Download failed: Received status", response.Status)
-			return
-		}
-
-		filename := path.Base(url) //(flags.URL)
-		if flags.OutputFile != "" {
-			filename = flags.OutputFile
-		}
-
-		// Determine file path
-		filepath := filename
-		if flags.Path != "" {
-			// fmt.Println("file path 1 : ", flags.Path)
-			if strings.HasPrefix(flags.Path, "~/") {
-				homeDir, err := os.UserHomeDir()
-				if err != nil {
-					fmt.Println("Error getting home directory:", err)
-					return
-				}
-				filepath = path.Join(homeDir, flags.Path[2:], filename)
-				// fmt.Println("file path is : ", filepath)
-				// return
-			} else {
-
-				err := os.MkdirAll(flags.Path, 0o777)
-				if err != nil {
-					fmt.Println("Error creating directory:", err)
-					return
-				}
-				filepath = path.Join(flags.Path, filename)
-			}
-			// fmt.Println("file path is : ", filepath)
-		}
-
-		// Create the output file
-		outFile, err := os.Create(filepath)
-		if err != nil {
-			fmt.Println("Error creating file:", err)
-			return
-		}
-		defer outFile.Close()
-
-		// Copy data from response to file
-		size, err := io.Copy(outFile, response.Body)
-		if err != nil {
-			fmt.Println("Error saving file:", err)
-			return
-		}
-
-		// // Convert size to appropriate unit (bytes, KB, MB, GB)
-		fileSizeStr := functions.FormatSize(size)
-
-		fmt.Printf("content size: %s\n", fileSizeStr)
-		fmt.Println("saving file to:", filepath)
-		fmt.Printf("Downloaded %s\n", flags.URL)
-
-		// Print finish time
-		endTime := time.Now()
-		fmt.Printf("finished at %s\n", endTime.Format("2006-01-02 15:04:05"))
-	}
-}
-
-func StartDownloads(args, urls []string, outputDir string) {
-
-	flags := &functions.ArgValues{}
-	flags.ParseFlags(args)
-	var wg sync.WaitGroup
-	for _, url := range urls {
 		wg.Add(1)
-		go func(u string) {
+		go func(url string) { // Run each download in a goroutine
 			defer wg.Done()
-			if flags.IsMirror {
-				MirrorWebsite(u, outputDir)
-			} else {
-				DownloadFile(u, "", outputDir)
-			}
+			DownloadFile(url, flags)
 		}(url)
 	}
-	wg.Wait()
+
+	wg.Wait() // Wait for all downloads to finish
 }
 
-func MirrorWebsite(baseURL, outputDir string) {
-	fmt.Println("Mirroring website:", baseURL)
-	htmlFilePath := DownloadFile(baseURL, "index.html", outputDir)
-	if htmlFilePath == "" {
-		fmt.Println("Failed to download main page.")
+// Function to handle file downloading
+func DownloadFile(url string, flags *functions.ArgValues) {
+	// fmt.Println("THIS IS THE URL", url)
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Print response status
+	fmt.Printf("status %s\n", response.Status)
+	if response.StatusCode != http.StatusOK {
+		fmt.Println("Download failed: Received status", response.Status)
 		return
 	}
 
-	resources := ExtractResources(htmlFilePath)
-	var wg sync.WaitGroup
-	for _, resource := range resources {
-		wg.Add(1)
-		go func(res string) {
-			defer wg.Done()
-			DownloadFile(res, "", outputDir)
-		}(resource)
-	}
-	wg.Wait()
-	fmt.Println("Website mirrored successfully.")
-}
-
-func ExtractResources(filePath string) []string {
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		fmt.Println("Error reading HTML file:", err)
-		return nil
-	}
-	var resources []string
-	re := regexp.MustCompile(`(src|href)="([^"]+\.(css|js|png|jpg|jpeg|gif|svg))"`)
-	for _, match := range re.FindAllStringSubmatch(string(data), -1) {
-		resources = append(resources, match[2])
-	}
-	return resources
-}
-
-func DownloadFile(url, outputFile, outputDir string) string {
-	fmt.Println("Downloading:", url)
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return ""
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Download failed: Received status", resp.Status)
-		return ""
+	filename := path.Base(url)
+	if flags.OutputFile != "" {
+		filename = flags.OutputFile
 	}
 
-	filename := outputFile
-	if filename == "" {
-		filename = path.Base(url)
+	// Determine file path
+	filepath := filename
+	if flags.Path != "" {
+		if strings.HasPrefix(flags.Path, "~/") {
+			homeDir, err := os.UserHomeDir()
+			if err != nil {
+				fmt.Println("Error getting home directory:", err)
+				return
+			}
+			filepath = path.Join(homeDir, flags.Path[2:], filename)
+		} else {
+			err := os.MkdirAll(flags.Path, 0o777)
+			if err != nil {
+				fmt.Println("Error creating directory:", err)
+				return
+			}
+			filepath = path.Join(flags.Path, filename)
+		}
 	}
 
-	if outputDir != "" {
-		os.MkdirAll(outputDir, os.ModePerm)
-		filename = path.Join(outputDir, filename)
-	}
-
-	outFile, err := os.Create(filename)
+	// Create the output file
+	outFile, err := os.Create(filepath)
 	if err != nil {
 		fmt.Println("Error creating file:", err)
-		return ""
+		return
 	}
 	defer outFile.Close()
 
-	_, err = io.Copy(outFile, resp.Body)
+	// Copy data from response to file
+	size, err := io.Copy(outFile, response.Body)
 	if err != nil {
 		fmt.Println("Error saving file:", err)
-		return ""
+		return
 	}
 
+	// Convert size to appropriate unit
+	fileSizeStr := functions.FormatSize(size)
+
+	fmt.Printf("content size: %s\n", fileSizeStr)
+	fmt.Println("saving file to:", filepath)
 	fmt.Printf("Downloaded %s\n", url)
-	return filename
+
+	// Print finish time
+	endTime := time.Now()
+	fmt.Printf("finished at %s\n", endTime.Format("2006-01-02 15:04:05"))
 }
-
-// var (
-// 	rateLimit  int64 = 0 // Default: No rate limit
-// 	mirrorMode bool  = false
-// )
-
-// func main() {
-// 	args := os.Args[1:]
-// 	if len(args) < 1 {
-// 		fmt.Println("Usage: go run main.go [FLAGS] <URL>")
-// 		return
-// 	}
-
-// 	outputDir, inputFile, background, urls := parseArguments(args)
-
-// 	if inputFile != "" {
-// 		fileUrls, err := readURLsFromFile(inputFile)
-// 		if err != nil {
-// 			fmt.Println("Error reading file:", err)
-// 			return
-// 		}
-// 		urls = append(urls, fileUrls...)
-// 	}
-
-// 	if len(urls) == 0 {
-// 		fmt.Println("Error: No URL provided")
-// 		return
-// 	}
-
-// 	if background {
-// 		enableBackgroundMode()
-// 	}
-
-// 	startDownloads(urls, outputDir)
-// }
