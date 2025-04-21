9c5330a039527d4400cde90bc8fb5d9d72eb64df
vandisi <vinolia.b.19@gmail.com>
2025-04-21 16:58:56 +0300

    fix: Remove unnecessary comments

--- cmd/functions/parseFiles.go
+++ cmd/functions/parseFiles.go
@@ -12,16 +12,12 @@ func (input *ArgValues) ParseFlags(args []string) {
 			input.Background = true
 		} else if strings.HasPrefix(arg, "-O=") {
 			input.OutputFile = arg[len("-O="):]
-			// fmt.Println("-0 flag passed")
 		} else if strings.HasPrefix(arg, "https://") || strings.HasPrefix(arg, "http://") || strings.HasPrefix(arg, "ftp://") {
 			input.HasFlag = true
-			// input.URL = arg
 			input.URL = append(input.URL, arg) // arg
-			// fmt.Println("valid url")
 		} else if strings.HasPrefix(arg, "-P=") {
 			input.Path = arg[len("-P="):]
-			// input.OutputFile = args[i+1]
-			fmt.Println("Path to save file")
+			// fmt.Println("Path to save file")
 		} else if strings.HasPrefix(arg, "--rate-limit=") {
 			if strings.HasSuffix(arg, "k") || strings.HasSuffix(arg, "m") || strings.HasSuffix(arg, "K") || strings.HasSuffix(arg, "M") {
 				input.RateLimit = arg[len("--rate-limit="):]
@@ -32,10 +28,8 @@ func (input *ArgValues) ParseFlags(args []string) {
 			}
 		} else if arg == "--mirror" {
 			input.IsMirror = true
-			fmt.Println("Instance of mirror")
 		} else if strings.HasPrefix(arg, "-i=") {
 			input.InputFile = arg[len("-i="):]
-			fmt.Println("Download different files from a file")
 		}
 	}
 }
