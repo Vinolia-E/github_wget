bbc15254e9722c654eaa5d081343fed4495fb524
vandisi <vinolia.b.19@gmail.com>
2025-04-21 13:33:34 +0300

    feat: Update function to ignore empty lines

--- cmd/functions/readFile.go
+++ cmd/functions/readFile.go
@@ -8,6 +8,8 @@ import (
 
 func ReadInputFile(filename string) []string {
 
+	var results []string
+
 	fileByte, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Println("Error reading the Input file: ", err)
@@ -15,7 +17,15 @@ func ReadInputFile(filename string) []string {
 	}
 
 	inputString := string(fileByte)
-	results := strings.Split(inputString, "\n")
+	splited := strings.Split(inputString, "\n")
+	// Remove empty lines
+	for _, lines := range splited {
+		if lines == "" {
+			continue
+		} else {
+			results = append(results, lines)
+		}
+	}
 
 	return results
 }
